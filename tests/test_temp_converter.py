ca8e8c8fac212f7b737288bf7ecb5e57a15908dc
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-05 13:21:43 -0400

    Update test_temp_converter.py

--- tests/test_temp_converter.py
+++ tests/test_temp_converter.py
@@ -2,8 +2,14 @@
 import pytest
 from temp_converter import c2f
 
-def test_c2f():
-    assert round(c2f(0), 2) == 32.0     # Freezing point of water (This should pass)
-    assert round(c2f(100), 2) == 211.0   # Boiling point of water (Expected to fail)
-    assert round(c2f(37.78), 2) == 99.0 # Very hot day (Expected to fail)
-    assert round(c2f(20), 2) == 68.0     # Normal temperature (Expected to fail)
+def test_c2f_pass():
+    assert round(c2f(0), 2) == 32.0     # This should pass
+
+def test_c2f_fail_1():
+    assert round(c2f(100), 2) == 211.0   # This should fail
+
+def test_c2f_fail_2():
+    assert round(c2f(37.78), 2) == 99.0 # This should fail
+
+def test_c2f_fail_3():
+    assert round(c2f(20), 2) == 68.0     # This should fail

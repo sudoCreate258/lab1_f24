562ec9596384904ccac3043ba833b465ccf86800
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-05 13:20:03 -0400

    Update test_temp_converter.py

--- tests/test_temp_converter.py
+++ tests/test_temp_converter.py
@@ -2,18 +2,8 @@
 import pytest
 from temp_converter import c2f
 
-def test_c2f_freezing_point():
-    # This test is expected to pass
-    assert round(c2f(0), 2) == 32.0  # Freezing point of water
-
-def test_c2f_boiling_point():
-    # This test is expected to fail
-    assert round(c2f(100), 2) == 212.0  # Boiling point of water
-
-def test_c2f_hot_day():
-    # This test is expected to fail
-    assert round(c2f(37.78), 2) == 100.0  # Very hot day
-
-def test_c2f_failure_case():
-    # This test is expected to fail
-    assert round(c2f(0), 2) == 31.0  # This should fail based on your function
+def test_c2f():
+    assert round(c2f(0), 2) == 32.0     # Freezing point of water (This should pass)
+    assert round(c2f(100), 2) == 211.0   # Boiling point of water (Expected to fail)
+    assert round(c2f(37.78), 2) == 99.0 # Very hot day (Expected to fail)
+    assert round(c2f(20), 2) == 68.0     # Normal temperature (Expected to fail)

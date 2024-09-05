388f5a17d73f0c1680d4ef925eaf256df7e85f29
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-05 12:36:27 -0400

    Update test_temp_converter.py

--- tests/test_temp_converter.py
+++ tests/test_temp_converter.py
@@ -1,13 +1,18 @@
 import unittest
-from temp_converter.temp_converter import c2f  # Import the function to be tested
+from temp_converter import c2f
 
 class TestTempConversion(unittest.TestCase):
+    def test_freezing_point(self):
+        self.assertAlmostEqual(c2f(0), 32, places=2)
 
-    def test_c2f(self):
-        # Test cases for c2f function
-        self.assertAlmostEqual(c2f(0), 32, places=2)    # Freezing point of water
-        self.assertAlmostEqual(c2f(100), 212, places=2)  # Boiling point of water
-        self.assertAlmostEqual(c2f(37.78), 100, places=2)  # Very hot day
+    def test_boiling_point(self):
+        self.assertAlmostEqual(c2f(100), 212, places=2)
 
-if __name__ == "__main__":
+    def test_hot_day(self):
+        self.assertAlmostEqual(c2f(37.78), 100, places=2)
+
+    def test_edge_case(self):
+        self.assertAlmostEqual(c2f(-40), -40, places=2)  # Freezing point of Celsius in Fahrenheit
+
+if __name__ == '__main__':
     unittest.main()

ba39239eec8e9c0d9dfa17a53cc3566c3c4f4b70
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-05 12:05:42 -0400

    Update test_temp_converter.py

--- tests/test_temp_converter.py
+++ tests/test_temp_converter.py
@@ -1,6 +1,6 @@
 import unittest
 import pytest
-from temp_converter.temp_converter import c2f  # Adjust to your actual module structure
+from src.temp_converter.temp_converter import c2f
 
 class TestTempConversion(unittest.TestCase):
     def test_c2f(self):

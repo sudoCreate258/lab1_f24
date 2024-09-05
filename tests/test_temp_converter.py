d24edc6a515bd8f5887e5085624a9a2cd5cc3028
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-05 12:54:22 -0400

    Update test_temp_converter.py

--- tests/test_temp_converter.py
+++ tests/test_temp_converter.py
@@ -1,6 +1,5 @@
 import pytest
-from temp_converter import c2f, f2c
-
+from temp_converter import c2f
 def test_c2f():
     assert round(c2f(0), 2) == 32  # Freezing point of water
     assert round(c2f(100), 2) == 212  # Boiling point of water
